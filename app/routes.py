482dc1b213c1bc7fdb5495b99ca7a651eade2fa7
Jay <ronokamau@.com>
2022-12-06 03:58:19 +0300

    some template changes

--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,28 @@
-from flask import render_template
+from flask import render_template, url_for
 from app import app
 
 @app.route('/index')
-@app.route('/')
 def index():
     return render_template('index.html', title='index')
 
 @app.route('/base')
 def base():
     return render_template('base.html', title='base')
+
+@app.route('/profile')
+def profile():
+    # code for rendering the user profile page
+    return render_template('profile.html')
+
+@app.route('/landing')
+@app.route('/')
+def landing():
+    return render_template('landing.html', title='landing page')
+
+@app.route('/login')
+def login():
+        return render_template('login.html', title='login')
+
+@app.route('/signup')
+def signup():
+        return render_template('signup.html', title='signup')
